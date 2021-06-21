ebafb3359d7a29916ebfbb72f268f5d5765d1d75
Cindy Qi Li <cli@ocadu.ca>
2021-06-21 16:52:18 -0400

    fix: improve issuePullRequest() and upgrade npm packages

--- scripts/gitOpsApi.js
+++ scripts/gitOpsApi.js
@@ -10,8 +10,6 @@ https://raw.githubusercontent.com/inclusive-design/data-update-github/main/LICEN
 
 "use strict";
 
-const axios = require("axios");
-
 module.exports = {
 	/**
 	 * An object that contains required information for fetching a file.
@@ -163,62 +161,62 @@ module.exports = {
 	},
 
 	/**
-	 * Issue pull request against the data repo on behalf of an issuer account
-	 * @param {String} githubGraphqlAPI - The URL of the Github GraphQL API
-	 * @param {String} issuerGithubId - The Github account that the pull request will be issued on behalf of
-	 * @param {String} issuerAccessToken - The personal access token of the Github account that the pull request will
-	 * be issued on behalf of
-	 * @param {String} branchName - The name of the remote branch
-	 * @param {String} publishedDate - The published date of the new data files. Used in the commit message
-	 * @return {String|Object} return the URL of the issued pull request when the pull request is issued successfully.
-	 * otherwise, return the error object in a structure of: {isError: true, message: detailed-error-message}
+	 * An object that contains required information for submitting multiple files in one commit.
+	 * @typedef {Object} IssuePullRequestOptions
+	 * @param {String} repoOwner - The repo owner.
+	 * @param {String} repoName - The repo name.
+	 * @param {String} issuerGithubId - The Github id of the issuer.
+	 * @param {String} branchName - The name of the remote branch to operate.
+	 * @param {String} prTitle - The pull request title.
 	 */
-	issuePullRequest: async (githubGraphqlAPI, issuerGithubId, issuerAccessToken, branchName, publishedDate) => {
-		const repoOwner = process.env.REPO_OWNER;
-		const repoName = process.env.REPO_NAME;
 
-		const headers = {
-			"Authorization": "bearer " + issuerAccessToken
-		};
-
-		const repoInfoResponse = await axios.post(githubGraphqlAPI, {
-			query: `query {
-			repository(owner: "${repoOwner}", name: "${repoName}") {
-				url
-				id
-			}
-			}`
-		}, {
-			headers:headers
+	/**
+	 * Issue pull request against the data repo on behalf of an issuer account
+	 * @param {Object} octokit - An instance of octokit with authentication being set.
+	 * @param {IssuePullRequestOptions} options - Required parameters for this github operation.
+	 * @return {Promise} The resolve or reject of the promise indicates whether the operation completes successfully.
+	 * When the pull request is issued successfully, the resolved value is the URL of the pull request. When it fails,
+	 * rejects with {isError: true, message: {String}}
+	 */
+	issuePullRequest: async (octokit, options) => {
+		return new Promise((resolve, reject) => {
+			octokit.graphql(
+				`query {
+					repository(owner: "${options.repoOwner}", name: "${options.repoName}") {
+						url
+						id
+					}
+				}`
+			).then((repoInfoResponse) => {
+				const repoId = repoInfoResponse.repository.id;
+				octokit.graphql(
+					`mutation {
+						createPullRequest (
+							input: {
+								baseRefName: "main",
+								headRefName: "${options.issuerGithubId}:${options.branchName}",
+								repositoryId: "${repoId}",
+								title: "${options.prTitle}"
+							}) {
+							pullRequest {
+							  url
+							}
+						  }
+					}`
+				).then((createPRResponse) => {
+					resolve(createPRResponse.createPullRequest.pullRequest.url);
+				}).catch((e) => {
+					reject({
+						isError: true,
+						message: e.message
+					});
+				});
+			}).catch((e) => {
+				reject({
+					isError: true,
+					message: e.message
+				});
+			});
 		});
-
-		const repoId = repoInfoResponse.data.data.repository.id;
-
-		const createPRResponse = await axios.post(githubGraphqlAPI, {
-			query: `mutation {
-			createPullRequest (
-				input: {
-					baseRefName:"main",
-					headRefName: "${issuerGithubId}:${branchName}",
-					repositoryId: "${repoId}",
-					title: "Add a new ODC data file published on ${publishedDate}"
-				}) {
-				pullRequest {
-				  url
-				}
-			  }
-			}`
-		}, {
-			headers:headers
-		});
-
-		if (createPRResponse.data.errors) {
-			return {
-				isError: true,
-				message: createPRResponse.data.errors
-			};
-		} else {
-			return createPRResponse.data.data.createPullRequest.pullRequest.url;
-		}
 	}
 };
